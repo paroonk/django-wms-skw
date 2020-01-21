7bdb42756a2d8c85c09835a5eaac1154b717cf69
paroonk <paroonk@hotmail.com>
2020-01-21 10:21:35 +0700

    Final

--- mysite/server.py
+++ mysite/server.py
@@ -1,8 +1,9 @@
 import logging
 import multiprocessing
 
-from mysite.wsgi import application
 from waitress import serve
+
+from mysite.wsgi import application
 
 logging.getLogger('waitress').setLevel(logging.ERROR)
 serve(application, listen="*:8000", threads=multiprocessing.cpu_count())
